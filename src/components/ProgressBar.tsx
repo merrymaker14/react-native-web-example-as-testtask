908cedc500a735aaec7a8f232bc57d0fa61b88ae
merrymaker14 <tmusharapov@bk.ru>
2019-08-15 19:14:54 +0300

    Init commit

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ProgressBar: React.FC = () => {
+  return (
+    <View>
+      <Text>It's used a library for a time</Text>
+    </View>
+  );
+};
+
+export default ProgressBar;
