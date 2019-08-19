8ef07d02190e2b7c0f9408f89fe5d5f0c552e1e6
merrymaker14 <tmusharapov@bk.ru>
2019-08-19 08:18:39 +0300

    Updates

---
+++ precache-manifest.3837869bbb16d517d92a600b8e20e521.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ff1325c5bdf62b6850b371a048da61",
+    "url": "/react-native-web-example-as-testtask/index.html"
+  },
+  {
+    "revision": "0a05d1cc7eb1ababc92a",
+    "url": "/react-native-web-example-as-testtask/static/js/2.d3d3ee1c.chunk.js"
+  },
+  {
+    "revision": "4b8f3ea08539b1846314",
+    "url": "/react-native-web-example-as-testtask/static/js/main.49e23ac8.chunk.js"
+  },
+  {
+    "revision": "301991de5a3f845995f9",
+    "url": "/react-native-web-example-as-testtask/static/js/runtime~main.e2a881ad.js"
+  },
+  {
+    "revision": "135d53c793a22e03f7e348214f1994d8",
+    "url": "/react-native-web-example-as-testtask/static/media/960x576_1.135d53c7.png"
+  },
+  {
+    "revision": "42d9b58d8e7e8e9773608be41cdd1cec",
+    "url": "/react-native-web-example-as-testtask/static/media/nc_ss19_620x960px_1.42d9b58d.png"
+  }
+]);
